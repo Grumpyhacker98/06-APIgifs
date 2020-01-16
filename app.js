361c0dff30fd05d55a14266988320e9c42919ca2
Max Lewis <max.a.lewis98@gmail.com>
2020-01-15 23:13:32 -0700

    scrapcode and first button is responding

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
 
+animals = ["cats","dogs","birds","fish"]
+
 // load the animal buttons from the animal button array
 // function run on page load and on when animals are added
 
 
 // onpageload
 
-// click an animal button
+// generate 10 new animal gifs
+$("button").on("click", function() {
 // grab 10 animal gifs from the website
 // give them indivigual identity classes when being created
 // print
+});
 
-// click an animal gif
-// un-pause the clicked gif
+// 06/16ish has good code for this
+// pause unpause animal gifs
+$("-----").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+});
+  
 
-// click the add an animal button
+// add an animal to animals[] 
+$("#animalAdd").click(function(event) {
+    event.preventDefault();
+
+    temp = $("#animalNew").val()
+    animals.push(temp)
+
+});
+
+// var myButton = $("<button>");
+// myButton.attr("data-my-name", "Bond, James Bond");
+// myButton.addClass("secret-agent");
+// myButton.attr("id", "double-O-seven");
+// myButton.text("Shaken, not stirred!");
+// $("#animal-gifs").append(myButton);
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response) {
+
+// });
+
+
+// code from the gif pause activity
+
 

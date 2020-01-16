ff7a00145f0b3373d011954775b4767bf3c60cb5
Max Lewis <max.a.lewis98@gmail.com>
2020-01-15 22:18:00 -0700

    html framework and psuedocode framework

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// load the animal buttons from the animal button array
+// function run on page load and on when animals are added
+
+
+// onpageload
+
+// click an animal button
+// grab 10 animal gifs from the website
+// give them indivigual identity classes when being created
+// print
+
+// click an animal gif
+// un-pause the clicked gif
+
+// click the add an animal button
+
